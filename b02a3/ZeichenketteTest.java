3a43958f1ff49d23e21b1e288603053cadd4b3a1
JonaZorn <zornjona@gmail.com>
2026-05-07 23:34:02 +0200

    Aufgabe2_2-3

---
+++ b02a3/ZeichenketteTest.java
@@ -0,0 +1,7 @@
+public class ZeichenketteTest{
+  public static void main(String[] args){
+    System.out.println(Zeichenkette.grussMitZeit(120,"Jona"));
+      System.out.println(Zeichenkette.zitronenEis(1));
+
+    }
+}
