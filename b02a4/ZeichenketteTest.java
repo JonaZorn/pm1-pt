c540664777ef28b7714e86f023c4ecd4f1bb7f89
JonaZorn <zornjona@gmail.com>
2026-05-08 23:48:08 +0200

    aufgabe2

---
+++ b02a4/ZeichenketteTest.java
@@ -0,0 +1,7 @@
+public class ZeichenketteTest{
+  public static void main(String[] args){
+    System.out.println(Zeichenkette.wiederhole(3));
+    System.out.println(Zeichenkette.wiederhole(3, "Humba", ','));
+
+    }
+}
