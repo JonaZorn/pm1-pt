ba811f8e01e9d4a9477c72c28847244ab33dab31
JonaZorn <zornjona@gmail.com>
2026-05-29 15:20:42 +0200

    java 5

---
+++ reste-aus-dem-prackrikum/Zeichenkette.java
@@ -0,0 +1,13 @@
+public class Zeichenkette{
+    public static String verkuerze(String s, int n){
+        int r1 = s.length();
+        if((n + n) < (r1 - 5)){
+            String s1 = s.substring(0,n);
+            String s2 = s.substring(r1 - n);
+            String zeichen = "[...]";
+            String zurück = s1 + zeichen + s2;
+            return zurück;
+        }
+        return s;
+    }
+}
